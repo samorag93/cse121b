e7673fe41bd9eb627f05d20cef1d3ccf1b06d708
samorag93 <moronyag90@gmail.com>
2023-10-19 20:00:33 +0200

    testing

--- scripts/finalScript.js
+++ scripts/finalScript.js
@@ -1,4 +1,4 @@
-import { getTriviaData } from './api.js';
+const TRIVIA_API_URL = 'https://raw.githubusercontent.com/samorag93/cse121b/main/scripts/question.json';
 
 let currentQuestionIndex = 0;
 let triviaData = [];
@@ -7,17 +7,14 @@ const questionText = document.getElementById("question-text");
 const optionsList = document.getElementById("options-list");
 const nextButton = document.getElementById("next-button");
 
-function displayQuestion() {
-    const currentQuestion = triviaData[currentQuestionIndex];
-    questionText.textContent = currentQuestion.question;
-    optionsList.innerHTML = "";
-
-    currentQuestion.options.forEach((option, index) => {
-        const li = document.createElement("li");
-        li.textContent = option;
-        li.addEventListener("click", () => checkAnswer(index));
-        optionsList.appendChild(li);
-    });
+async function getTriviaData() {
+    try {
+        const response = await fetch(TRIVIA_API_URL);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw new Error("Error al obtener los datos de trivia: " + error);
+    }
 }
 
 async function loadTriviaData() {
@@ -29,6 +26,21 @@ async function loadTriviaData() {
     }
 }
 
+function displayQuestion() {
+    const currentQuestion = triviaData[currentQuestionIndex];
+    questionText.textContent = currentQuestion.question;
+    optionsList.innerHTML = "";
+
+    currentQuestion.options.forEach((option, index) => {
+        const li = document.createElement("li");
+        li.textContent = option;
+        li.addEventListener("click", () => {
+            checkAnswer(index); // Verifica la respuesta del usuario al hacer clic en una opción
+        });
+        optionsList.appendChild(li);
+    });
+}
+
 function checkAnswer(selectedOption) {
     const currentQuestion = triviaData[currentQuestionIndex];
     if (selectedOption === currentQuestion.answer) {
@@ -36,15 +48,7 @@ function checkAnswer(selectedOption) {
         alert("¡Respuesta correcta!");
     } else {
         // La respuesta es incorrecta
-        alert("Respuesta incorrecta. Inténtalo de nuevo.");
-    }
-
-    currentQuestionIndex++;
-    if (currentQuestionIndex < triviaData.length) {
-        displayQuestion();
-    } else {
-        alert("Has respondido todas las preguntas. ¡Juego terminado!");
-        // Aquí puedes reiniciar el juego o realizar alguna otra acción
+        alert("Respuesta incorrecta. La respuesta correcta es: " + currentQuestion.options[currentQuestion.answer]);
     }
 }
 
