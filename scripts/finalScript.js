847e46600dfb8e8b591d1fb9acb7e25372519bf1
samorag93 <moronyag90@gmail.com>
2023-10-24 18:26:54 +0200

    final project quiz

--- scripts/finalScript.js
+++ scripts/finalScript.js
@@ -6,6 +6,9 @@ let triviaData = [];
 const questionText = document.getElementById("question-text");
 const optionsList = document.getElementById("options-list");
 const nextButton = document.getElementById("next-button");
+const prevButton = document.getElementById("prev-button");
+let lastQuestion= -1;
+
 
 async function getTriviaData() {
     try {
@@ -26,6 +29,11 @@ async function loadTriviaData() {
     }
 }
 
+const scoreLabel = document.getElementById("score");
+const hintLabel = document.querySelector(".hint");
+
+let score = 0;
+
 function displayQuestion() {
     const currentQuestion = triviaData[currentQuestionIndex];
     questionText.textContent = currentQuestion.question;
@@ -37,29 +45,91 @@ function displayQuestion() {
         li.addEventListener("click", () => {
             checkAnswer(index); // Verifica la respuesta del usuario al hacer clic en una opción
         });
+        li.addEventListener("mouseover", () => {
+            li.style.cursor = "pointer";
+        })
         optionsList.appendChild(li);
     });
+    updateScore();
+    const resultLabel = document.getElementById("result-label");
+    const hintLabel = document.querySelector(".hint");
+    resultLabel.style.display = "none";
+    hintLabel.style.display = "none";
 }
+
+let answered = false;
 
 function checkAnswer(selectedOption) {
     const currentQuestion = triviaData[currentQuestionIndex];
-    if (selectedOption === currentQuestion.answer) {
-        // La respuesta es correcta
-        alert("¡Respuesta correcta!");
+    const resultLabel = document.querySelector("#result-label");
+    const hintLabel = document.querySelector(".hint");
+    
+    if (!answered)
+    {
+        
+        if (selectedOption === currentQuestion.answer) {
+            resultLabel.textContent = "Correct Answer!"
+            resultLabel.style.display = "block"
+            hintLabel.style.display ="none";
+            score++;
+            
+        } else {
+            resultLabel.textContent = `Wrong answer, the correct answer is: ${currentQuestion.options[currentQuestion.answer]}`;
+            resultLabel.style.display = "block";
+            hintLabel.textContent = `Track: ${currentQuestion.track}`;
+            hintLabel.style.display = "block";
+        }
+        answered = true;
+        updateScore();
     } else {
-        // La respuesta es incorrecta
-        alert("Respuesta incorrecta. La respuesta correcta es: " + currentQuestion.options[currentQuestion.answer]);
+        resultLabel.textContent = "You already answered this question";
+        resultLabel.style.display = "block";
+        hintLabel.style.display = "none";
+        answered = false;
+    }
+    
+}
+
+function nextQuestion() {
+    answered = false;
+    currentQuestionIndex++;
+    if (currentQuestionIndex < triviaData.length) {
+        displayQuestion();
+    } else {
+        displayEndMessage();
     }
 }
+
+
+function updateScore() {
+    const scoreLabel = document.querySelector("#score");
+    scoreLabel.textContent = `${score}`;
+}
+
+prevButton.addEventListener("click", () => {
+    if (currentQuestionIndex > 0) {
+        currentQuestionIndex--;
+        displayQuestion();
+        const resultLabel = document.getElementById("result-label");
+        resultLabel.style.display = "none"; // Oculta la etiqueta de respuesta al retroceder a la pregunta anterior
+    }
+});
 
 nextButton.addEventListener("click", () => {
     currentQuestionIndex++;
     if (currentQuestionIndex < triviaData.length) {
         displayQuestion();
     } else {
-        alert("Has respondido todas las preguntas. ¡Juego terminado!");
+        if (score < 5) {
+            alert("Your score is too low try again");
+        }else {
+            alert("Has respondido todas las preguntas. ¡Juego terminado!");
         // Aquí puedes reiniciar el juego o realizar alguna otra acción
+        }
     }
 });
+
+
+
 
 loadTriviaData();
